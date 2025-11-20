1c4534f8f16f96edc25af8e000974b7061ed62b3
Anank1ng <35679021+Anank1ng@users.noreply.github.com>
2025-11-20 15:55:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.set_page_config(
     layout="wide",
 )
 
-st.title("📦 Dashboard Open Transfer Order PROD ➜ FG")
-st.caption("Sumber: File CSV Open Transfer Order PROD to FG")
+st.title("📦 Dashboard Open Transfer Order SEKAR LAUT")
+st.caption("Sumber: Report SKLT INV Transfer Order")
 
 # --------------------------------------
 # FUNGSI BACA CSV
